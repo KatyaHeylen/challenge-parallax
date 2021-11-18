39d154c348d7eb93728babc89ae17a33caa8be3c
Katya Heylen <ezhgurova@gmail.com>
2021-11-18 10:42:26 +0100

    changed css settings due to use background properties, completed parallax effect

--- script/script.js
+++ script/script.js
@@ -1,48 +1,46 @@
 const tic = 50;
 let x = 0;
 let y = 0;
-const moverate = 10;
+let keys = [];
 let character = document.getElementById("character");
 let treesbushes = document.getElementById("trees");
 let distanttrees = document.getElementById("distant_trees");
 let clouds = document.getElementById("clouds");
 let ground = document.getElementById('ground');
 
-window.addEventListener('keydown', function (event) {
-    if (event.code === "Space"){
-        console.log("jump");
-        makeJump ();
-    } else if (event.code === "ArrowLeft"){
-        console.log('Walking left');
-        character.style.transform = 'scaleX(-1)';
-        changePosition(-x);
-    } else if (event.code === "ArrowRight"){
-        console.log('Walking right');
-        character.style.transform = 'scaleX(1)';
-        changePosition(x);
+document.addEventListener('keydown', function (event) {
+    if (event.key && !keys.includes(event.key)) {
+        keys.push(event.key);
+        console.log(keys);
+        changePosition();
     }
-}, true);
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.key) {
+        var index = keys.indexOf(event.key);
+        if (index !== -1) {
+            keys.splice(index, 1);
+        }
+    }
+});
 
 function changePosition() {
-ground += 10;
-setTimeout(changePosition, tic);
+    if (keys.includes('ArrowRight')) {
+        x -= 1;
+        character.style.transform = 'scaleX(1)';
+    }
+    if (keys.includes('ArrowLeft')) {
+        x += 1;
+        character.style.transform = 'scaleX(-1)';
+    }
+    ground.style.backgroundPositionX = 4 * x + "px";
+    treesbushes.style.backgroundPositionX = 3 * x + "px";
+    distanttrees.style.backgroundPositionX = 2 * x + "px";
+    clouds.style.backgroundPositionX = x + "px";
+    setTimeout(changePosition, tic);
 }
 
 
-// document.addEventListener('keydown', function (event) {
-//     if (event.key && !keys.includes(event.key)) {
-//         keys.push(event.key);
-//         console.log(keys);
-//         changePosition();
-//     }
-// });
-//
-// document.addEventListener('keyup', function (event) {
-//     if (event.key) {
-//         let index = keys.indexOf(event.key);
-//         if (index !== -1) {
-//             keys.splice(index, 1);
-//             console.log(keys);
-//         }
-//     }
-// });
+
+
