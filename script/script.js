782560528c77a76d6d1745f144a30a3ad3b45aac
Katya Heylen <ezhgurova@gmail.com>
2021-11-18 15:31:00 +0100

    added up and down + spirit for fly

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-const tic = 50;
+const tic = 30;
 let x = 0;
 let y = 0;
 let keys = [];
@@ -9,12 +9,15 @@ let clouds = document.getElementById("clouds");
 let ground = document.getElementById('ground');
 
 changeSpirit();
+changePosition();
+fly();
+fallingDown();
+
 
 document.addEventListener('keydown', function (event) {
     if (event.key && !keys.includes(event.key)) {
         keys.push(event.key);
         console.log(keys);
-        changePosition();
         changeSpirit();
     }
 });
@@ -38,19 +41,41 @@ function changePosition() {
         x += 1;
         character.style.transform = 'scaleX(1)';
     }
-    ground.style.backgroundPositionX = 4 * x + "px";
+    ground.style.backgroundPositionX = 5 * x + "px";
     treesbushes.style.backgroundPositionX = 3 * x + "px";
     distanttrees.style.backgroundPositionX = 2 * x + "px";
     clouds.style.backgroundPositionX = x + "px";
     setTimeout(changePosition, tic);
 }
+
+function fly () {
+    if (keys.includes('ArrowUp')) {
+       y += 6;
+    }
+    character.style.bottom = 37 + y + "px";
+    setTimeout(fly, tic);
+}
+
+function fallingDown () {
+    if (keys.includes('ArrowDown')) {
+        y -= 8;
+    }
+    character.style.bottom = y + "px";
+    setTimeout(fallingDown, tic);
+}
+
 function changeSpirit() {
-    if (keys.length !== 0) {
-    character.style.backgroundImage = "url(../images/walking.gif)";
-    character.style.backgroundSize = "260px";
-    } else {
+    if (keys.length === 0) {
     character.style.backgroundImage = "url(../images/idle.gif)";
     character.style.backgroundSize = "100px";
+    }
+    else if (keys.includes("ArrowUp")) {
+    character.style.backgroundImage = "url(../images/fly.png)";
+    character.style.backgroundSize = "155px";
+    }
+    else {
+    character.style.backgroundImage = "url(../images/walking.gif)";
+    character.style.backgroundSize = "260px";
     }
 }
 
